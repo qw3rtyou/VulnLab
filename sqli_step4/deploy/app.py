7008d8818599ab421a35ef27ad2ed9d045323e0c
Meonu <yeonugim42@kyonggi.ac.kr>
2024-04-25 07:17:17 +0000

    add sqli_step5

---
+++ sqli_step4/deploy/app.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+import os
+import string
+
+app = Flask(__name__)
+
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+LOGIN_FLAG = os.getenv("LOGIN_FLAG")
+MYSQL_HOST = "db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    query = None
+    if request.method == "POST":
+        filtering_list = ['or', 'and', '"', ' ', '=', '<', '>', '\\']
+        username = request.form["username"].lower()
+        password = request.form["password"].lower()
+
+        for filter in filtering_list :
+            if (filter in username) or (filter in password):
+                return render_template("login.html", error='no Hack!')
+
+        query = text(f"SELECT * FROM user WHERE username = '{username}' AND password = '{password}'")
+        
+        with db.engine.connect() as connection:
+            result = connection.execute(query)
+            user = result.fetchone()
+        if user and user[1] == "admin":
+            return redirect(url_for("success", username=user[1], flag=LOGIN_FLAG))
+        elif user:
+            return redirect(url_for("success", username=user[1]))
+        else:
+            error = "Invalid Credentials"
+        return render_template("login.html", error=error, query=str(query))
+    else :
+        return render_template("login.html")
+
+
+@app.route("/success")
+def success():
+    username = request.args.get("username", "Unknown")
+    flag = request.args.get("flag", "So.. what?")
+    return render_template("success.html", username=username, flag=flag)
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True, host="0.0.0.0", port=8080)
